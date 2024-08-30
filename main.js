f1384836ccebd2080554a52605ae7a0e04adc58d
hamzapratama000 <hamzapratama000@gmail.com>
2024-08-30 08:10:13 +0000

    menambahkan fitur memori chat kegemini api

--- main.js
+++ main.js
@@ -14,6 +14,17 @@ let form = document.querySelector('form');
 let promptInput = document.querySelector('input[name="prompt"]');
 let output = document.querySelector('.output');
 
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash", // or gemini-1.5-pro
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+  ],
+});
+
 const chat = model.startChat({
   history: [],
   generationConfig:{
@@ -33,29 +44,31 @@ form.onsubmit = async (ev) => {
     //   .then(a => Base64.fromByteArray(new Uint8Array(a)));
 
     // Assemble the prompt by combining the text with the chosen image
-    let contents = [
-      {
-        role: 'user',
-        parts: [
-          // { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
-          { text: promptInput.value }
-        ]
-      }
-    ];
+    // let contents = [
+    //   {
+    //     role: 'user',
+    //     parts: [
+    //       // { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
+    //       { text: promptInput.value }
+    //     ]
+    //   }
+    // ];
 
     // Call the multimodal model, and get a stream of results
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash", // or gemini-1.5-pro
-      safetySettings: [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        },
-      ],
-    });
+    // const genAI = new GoogleGenerativeAI(API_KEY);
+    // const model = genAI.getGenerativeModel({
+    //   model: "gemini-1.5-flash", // or gemini-1.5-pro
+    //   safetySettings: [
+    //     {
+    //       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    //       threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    //     },
+    //   ],
+    // });
 
-    const result = await model.generateContentStream({ contents });
+    const prompt = promptInput.value;
+
+    const result = await chat.sendMessageStream(prompt);
 
     // Read from the stream and interpret the output as markdown
     let buffer = [];
