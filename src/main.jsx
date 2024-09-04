8716755cf66fa11c726b7bdef3f7010d23036c63
Farhan FR <refar03@gmail.com>
2024-09-04 15:25:43 +0700

    Make UI and add pop up

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 import App from "./app";
-import './index.css'
+import './index.css';
 
 const root = createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins']
+      },
+    },
+    screens: {
+      'ph': '320px',
+      //=> phone
+      'ml': '425px',
+      //=> mobile large
+      ...defaultTheme.screens,
+    },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
