cd9e70dbfbbbc259ea7f849f4351232de4132645
Fadil Efdika <94332351+fadilefdika@users.noreply.github.com>
2024-09-05 13:21:50 +0700

    Revert "Farhan Front End"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
-import App from './App'
-import './index.css';
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '../public/style.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
