b8b6795f3e9f58b1b911fadd393c2897baaf466b
fadilefd1102 <fadilefd1102@gmail.com>
2024-09-05 06:33:33 +0000

    Menyimpan perubahan pada main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
-import App from "./app";
-import './index.css'
+import App from './App'
+import './index.css';
 
-const root = createRoot(document.getElementById("root")).render(
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
